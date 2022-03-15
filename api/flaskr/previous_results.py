719040f354671ac80dcfc04c381f0804bf17f2f7
Lan Le <lanle@hotmail.nl>
2022-03-15 09:17:39 +0100

    list mock result storage (very basic)
    server only

--- api/flaskr/previous_results.py
+++ api/flaskr/previous_results.py
@@ -1,7 +1,7 @@
 # This program has been developed by students from the bachelor Computer Science at
 # Utrecht University within the Software Project course.
 # © Copyright Utrecht University (Department of Information and Computing Sciences)
-import csv # TODO fix this import
+import csv  # TODO fix this import
 from csv import writer
 import time
 import pandas as pd
@@ -10,49 +10,42 @@ from flask import (Blueprint, request)
 results_bp = Blueprint('results', __name__, url_prefix='/all-results')
 
 # Temporary list with results of calculations
- calculations = [
+results = [
     {
         'timestamp': '0',
         'number': '0',
         'reverse': ''
     }
- ]
-
-MAX_TEMP = 100
-#results_df = pd.DataFrame()
+]
 
 
-@results_bp.route('/results', methods=['GET', 'POST'])
+@results_bp.route('/results', methods=['GET'])
 def results():
-    #return results_df.to_dict('index')
+    return load_results()
 
 
 def save_result(number, reverse):
     timestamp = time.time()
-    add_csv_row([timestamp,number,reverse])
-    pd.read_csv()
-
-
-def newest_result():
-    #return results_df.tail(1)
+    add_csv_row([timestamp, number, reverse], 'results.tsv')
 
 
 def load_results():
-    df = pd.read_csv('test_results/magic_mock.tsv').tail(MAX_TEMP)
-    results = df.to_dict()
-    #global results_df  # TODO class instead of global
-    #results_df = df.tail(MAX_TEMP)
-    #print(results_df)
+    #path = 'test_results/magic_mock.tsv'
+    path = 'results.tsv'
+    return list_from_csv(path)
 
-def add_csv_row(list):
+
+def add_csv_row(row_list, path):
     # Open our existing CSV file in append mode
     # Create a file object and write a row to it
-    with open('event.csv', 'a') as file:
+    with open(path, 'a') as file:
         file_writer = writer(file)
         file_writer.writerow(list)
         file.close()
 
-def dict_from_csv():
-    with open('csv_file.csv', mode='r') as file:
+
+def list_from_csv(path):
+    with open(path, mode='r') as file:
         reader = csv.reader(file)
-        return {rows[0]: rows[1] for rows in reader}
+        rows = list(reader)
+        return rows
