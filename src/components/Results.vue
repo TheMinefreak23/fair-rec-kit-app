3c004b96589ea18c887c007c33dc1fbc29b53537
DiedevanderHoorn <35234641+DiedevanderHoorn@users.noreply.github.com>
2022-03-17 13:58:28 +0100

    tags and computation title
    Added dynamic tags and computation title to the results tab

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -7,7 +7,8 @@ import Table from './Table.vue'
 import { ref } from 'vue'
 
 const results = ref([
-  { dataset: 'LFM-1b', algorithm: 'ALS', hellinger_distance: '0.0988' , precision_p1 : '0.4505' }
+  { dataset: 'LFM-1b', algorithm: 'ALS', hellinger_distance: '0.0988' , precision_p1 : '0.4505' },
+  { dataset: 'LFM-1b', algorithm: 'POP', hellinger_distance: '0.1577' , precision_p1 : '0.1033' }
 ])
 
 const recommendations = ref([
@@ -18,18 +19,28 @@ const headers = ref(['dataset', 'algorithm', 'hellinger_distance', 'precision_p1
 const headers_rec = ref(['user_id', 'rec_item1', 'rec_item2'])
 const pretty_headers = ref(['Dataset', 'Algorithm', 'Hellinger distance', 'Precision p1@k'])
 const pretty_headers_rec= ref([ 'User id', 'First recommended item', 'Second recommended item'])
+
+const computation_name = ref('computation1')
+const computation_tags = ref(['tag1 ', 'tag2 ', 'tag3 ', 'tag4 '])
+
 </script>
 
 <template>
   <h1 class="display-2"> Results</h1>
   <p class="lead">
-    These are the results for your computation.
+    These are the results for your computation with the following name: {{computation_name}}.
+  </p>
+  
+  <p>
+    Tags: 
+      <tags v-for="tag in computation_tags"> {{tag}} </tags>
   </p>
 
   <div class = "container">
     <Table :results="results" :headers="headers" :pretty_headers="pretty_headers"/>
   </div>
 
+  <h6> Recommended items per user for dataset x and algorithm y </h6>
   <div class="container">
     <Table :results="recommendations" :headers="headers_rec" :pretty_headers="pretty_headers_rec"/>
   </div>
