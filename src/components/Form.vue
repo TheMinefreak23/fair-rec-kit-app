c1e5914d271094e7137ad7ee14ce37f35a9ee78d
Lan Le <lanle@hotmail.nl>
2022-03-20 11:40:32 +0100

    split method (radio group) fix

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -8,7 +8,7 @@ import FormGroupList from './FormGroupList.vue'
 const result = ref({})
 const options = ref()
 const form = ref({
-  splitMethod: [{ text: 'Random', value: 'random' }], //The default split method.
+  splitMethod: 'random', //The default split method.
 })
 
 onMounted(async () => {
@@ -53,7 +53,7 @@ async function initForm() {
   form.value = {}
   form.value.recommendations = options.value.defaults.recCount.default
   form.value.split = options.value.defaults.split
-  form.splitMethod = [{ text: 'Random', value: 'random' }]
+  form.value.splitMethod = 'random'
   //form.value.result.value = {}
 }
 </script>
@@ -62,36 +62,41 @@ async function initForm() {
   <b-card>
     <!--This form contains all the necessary parameters for a user to submit a request for a computation-->
     <b-form v-if="options" @submit="sendToServer" @reset="initForm">
-      <!--User can select a dataset. TODO: Multiple Datasets-->
-      <h2>Dataset</h2>
-      <b-form-group label="Select a dataset">
-        <!-- Select a dataset from the options received from the server -->
-        <b-form-select
-          v-model="form.dataset"
-          :options="[{ text: 'Choose...', value: null }, ...options.datasets]"
-          required
-        ></b-form-select>
-      </b-form-group>
+      <b-row>
+        <b-col>
+          <!--User can select a dataset. TODO: Multiple Datasets-->
+          <h2>Dataset</h2>
+          <b-form-group label="Select a dataset">
+            <!-- Select a dataset from the options received from the server -->
+            <b-form-select
+              v-model="form.dataset"
+              :options="[
+                { text: 'Choose...', value: null },
+                ...options.datasets,
+              ]"
+              required
+            ></b-form-select>
+          </b-form-group>
 
-      <!--User can select an optional filter-->
-      <b-form-group label="Select a filter">
-        <!-- Select a dataset filter from the options received from the server -->
-        <b-form-select
-          v-model="form.filter"
-          :options="[{ text: 'None (default)', value: null }]"
-        ></b-form-select>
-      </b-form-group>
+          <!--User can select an optional filter-->
+          <b-form-group label="Select a filter">
+            <!-- Select a dataset filter from the options received from the server -->
+            <b-form-select
+              v-model="form.filter"
+              :options="[{ text: 'None (default)', value: null }]"
+            ></b-form-select>
+          </b-form-group>
 
-      <!--User provides an optional rating conversion-->
-      <b-form-group label="Select a rating conversion">
-        <!-- Select a rating conversion from the options received from the server -->
-        <b-form-select
-          v-model="form.conversion"
-          :options="[{ text: 'None (default)', value: null }]"
-        ></b-form-select>
-      </b-form-group>
+          <!--User provides an optional rating conversion-->
+          <b-form-group label="Select a rating conversion">
+            <!-- Select a rating conversion from the options received from the server -->
+            <b-form-select
+              v-model="form.conversion"
+              :options="[{ text: 'None (default)', value: null }]"
+            ></b-form-select>
+          </b-form-group>
 
-      <!--
+          <!--
       <!-User can select which recommender approaches they want->
       <h2>Recommenders</h2>
       <b-form-group>
@@ -108,15 +113,15 @@ async function initForm() {
         ></b-form-checkbox-group>
       </b-form-group>-->
 
-      <b-form-group label="Select recommender approaches:"> </b-form-group>
-      <FormGroupList
-        @formChange="(x) => (form.approaches = x)"
-        name="approach"
-        plural="Recommender approaches"
-        selectName="an approach"
-        :options="options.approaches"
-      />
-      <!--
+          <b-form-group label="Select recommender approaches:"> </b-form-group>
+          <FormGroupList
+            @formChange="(x) => (form.approaches = x)"
+            name="approach"
+            plural="Recommender approaches"
+            selectName="an approach"
+            :options="options.approaches"
+          />
+          <!--
         User gets additional options for the selected recommender approaches
         <b-form-group 
           label="ALS Features value:" 
@@ -140,84 +145,91 @@ async function initForm() {
       </b-form-group>
       -->
 
-      <!--User can select the amount of recommendations per user -->
-      <b-form-group label="Select number of recommendations per user:">
-        <b-form-input
-          type="range"
-          :min="options.defaults.recCount.min"
-          :max="options.defaults.recCount.max"
-          v-model="form.recommendations"
-        ></b-form-input>
-        <p>{{ form.recommendations }}</p>
-        <b-form-checkbox
-          v-model="form.includeRatedItems"
-          buttons
-          button-variant="outline-primary"
-          required
-          >Include already rated items in recommendations</b-form-checkbox
-        >
-      </b-form-group>
+          <!--User can select the amount of recommendations per user -->
+          <b-form-group label="Select number of recommendations per user:">
+            <b-form-input
+              type="range"
+              :min="options.defaults.recCount.min"
+              :max="options.defaults.recCount.max"
+              v-model="form.recommendations"
+            ></b-form-input>
+            <p>{{ form.recommendations }}</p>
+            <b-form-checkbox
+              v-model="form.includeRatedItems"
+              buttons
+              button-variant="outline-primary"
+              required
+              >Include already rated items in recommendations</b-form-checkbox
+            >
+          </b-form-group>
+        </b-col>
+        <b-col>
+          <!--Input for train/test split-->
+          <h2>Train/test-split</h2>
+          <b-form-group label="Select test/train split:">
+            <b-form-input
+              type="range"
+              min="0"
+              max="100"
+              step="5"
+              id="customRange"
+              v-model="form.split"
+            ></b-form-input>
+            <p>Train: {{ form.split }}</p>
+            <p>Test: {{ 100 - form.split }}</p>
+          </b-form-group>
 
-      <!--Input for train/test split-->
-      <h2>Train/test-split</h2>
-      <b-form-group label="Select test/train split:">
-        <b-form-input
-          type="range"
-          min="0"
-          max="100"
-          step="5"
-          id="customRange"
-          v-model="form.split"
-        ></b-form-input>
-        <p>Train: {{ form.split }}</p>
-        <p>Test: {{ 100 - form.split }}</p>
+          <!--User can choose between a random and time-based train/testsplit-->
+          <b-form-group>
+            <b-form-radio-group v-model="form.splitMethod">
+              <b-form-radio value="random">Random (default)</b-form-radio>
+              <b-form-radio value="timesplit">Timesplit</b-form-radio>
+            </b-form-radio-group>
+          </b-form-group>
 
-        <!--User can choose between a random and time-based train/testsplit-->
-        <b-form-radio v-model="form.splitMethod">Random (default)</b-form-radio>
-        <b-form-radio v-model="form.splitMethod">Timesplit</b-form-radio>
-      </b-form-group>
+          <!--Input for metrics, user can add infinite metrics -->
+          <FormGroupList
+            @formChange="(x) => (form.metrics = x)"
+            name="metric"
+            plural="metrics"
+            selectName="a metric"
+            :options="options.metrics"
+          />
 
-      <!--Input for metrics, user can add infinite metrics -->
-      <FormGroupList
-        @formChange="(x) => (form.metrics = x)"
-        name="metric"
-        plural="metrics"
-        selectName="a metric"
-        :options="options.metrics"
-      />
+          <!--Input for results filter -->
+          <b-form-group label="Select a results filter">
+            <b-form-select
+              v-model="form.resFilter"
+              :options="[{ text: 'Global (default)', value: null }]"
+            ></b-form-select>
+          </b-form-group>
 
-      <!--Input for results filter -->
-      <b-form-group label="Select a results filter">
-        <b-form-select
-          v-model="form.resFilter"
-          :options="[{ text: 'Global (default)', value: null }]"
-        ></b-form-select>
-      </b-form-group>
-
-      <!-- Input for metadata such as:
+          <!-- Input for metadata such as:
      Computation Name
      Optional Tags
      Optional Email for notification -->
-      <h2>Meta</h2>
-      <b-form-group label="Enter name for computation">
-        <b-form-input
-          placeholder="New Computation"
-          v-model="form.name"
-          required
-        ></b-form-input>
-      </b-form-group>
-      <b-form-group label="Enter tags (optional)">
-        <b-form-input v-model="form.tags"></b-form-input>
-      </b-form-group>
-      <b-form-group label="Enter e-mail (optional)">
-        <b-form-input
-          placeholder="example@mail.com"
-          v-model="form.email"
-        ></b-form-input>
-      </b-form-group>
+          <h2>Meta</h2>
+          <b-form-group label="Enter name for computation">
+            <b-form-input
+              placeholder="New Computation"
+              v-model="form.name"
+              required
+            ></b-form-input>
+          </b-form-group>
+          <b-form-group label="Enter tags (optional)">
+            <b-form-input v-model="form.tags"></b-form-input>
+          </b-form-group>
+          <b-form-group label="Enter e-mail (optional)">
+            <b-form-input
+              placeholder="example@mail.com"
+              v-model="form.email"
+            ></b-form-input>
+          </b-form-group>
 
-      <b-button type="submit" variant="primary">Send</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
+          <b-button type="submit" variant="primary">Send</b-button>
+          <b-button type="reset" variant="danger">Reset</b-button>
+        </b-col>
+      </b-row>
     </b-form>
   </b-card>
 </template>
