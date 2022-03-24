24f0ba8f9a4b52e6daae29775a1fff14cbcd9623
LCYC <w>
2022-03-24 17:51:52 +0100

    file loads at least...
    file loading should happen internally. Now the user provides a file, but this is fine as a start to not stay stuck while having nothing ó_ò

--- src/components/Documentation.vue
+++ src/components/Documentation.vue
@@ -1,14 +1,37 @@
-<script>
-    /*This program has been developed by students from the bachelor Computer Science at
-    Utrecht University within the Software Project course.
-    © Copyright Utrecht University (Department of Information and Computing Sciences)*/
+<script setup>
+  /*This program has been developed by students from the bachelor Computer Science at
+  Utrecht University within the Software Poject course.
+  © Copyright Utrecht University (Department of Information and Computing Sciences)*/
+  /*
+  Documentation tab which shows all algorithms, metrics, datasets, etc. and their meaning.
+  */
+  function previewFile() {
+    const content = document.querySelector('.content');
+    const [file] = document.querySelector('input[type=file]').files;
+    const reader = new FileReader();
+
+    reader.addEventListener("load", () => {
+      // this will then display a text file
+      content.innerText = reader.result;
+    }, false);
+
+    if (file) {
+      reader.readAsText(file);
+    }
+  }
+ 
 </script>
 
 <template>
-    <div class="text-center py-2 mx-5">
-        <h3>Documentation</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rerum qui facilis. Perspiciatis officiis debitis accusamus illum harum sit dolore adipisci voluptatum. Rerum, velit quia magnam quis placeat necessitatibus ea.</p>
-    </div>
+  <div class="text-center py-2 mx-5">
+    <h3>Documentation</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rerum qui facilis. Perspiciatis officiis debitis accusamus illum harum sit dolore adipisci voluptatum. Rerum, velit quia magnam quis placeat necessitatibus ea.</p>
+  </div>
+  
+  <input type="file" v-on:change=previewFile()><br>
+  <p class="content"></p>
+  
+  
 </template>
 
 
