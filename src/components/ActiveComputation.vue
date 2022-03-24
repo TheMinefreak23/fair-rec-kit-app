e788f7b8d15e4a333a48abf4a9d8a245aef3f707
AlexC <aleksej.cornelissen@hotmail.com>
2022-03-24 14:06:15 +0100

    Added events for active computation

--- src/components/ActiveComputation.vue
+++ src/components/ActiveComputation.vue
@@ -2,20 +2,42 @@
     /*This program has been developed by students from the bachelor Computer Science at
     Utrecht University within the Software Project course.
     © Copyright Utrecht University (Department of Information and Computing Sciences)*/
+
+import { onMounted, ref } from 'vue'
+
+const emit = defineEmits(['computing', 'done', 'stop'])
 const props = defineProps({
   name: String,
 })
 
-function cancelComputation() {
 
+const computations = ref([])
+
+onMounted(() => {
+  getResults()
+})
+
+
+async function cancelComputation() {
+
+  emit('stop')
 }
+
+async function getComputations(){
+  const response = await fetch('http://localhost:5000/activecomputations')
+  const data = await response.json()
+  //if active computations:
+  emit('computing')
+}
+
+var done;
 
 </script>
 
 <template>
     <div>
       <h1>Waiting for "{{name}}" to be finished...</h1>
-      <b-img src="/Loading_icon.gif" fluid alt="Loading"></b-img>
+      <b-spinner></b-spinner> 
       <b-button v-b-modal.popup variant="danger">Cancel</b-button>
       <b-modal id="popup" 
                title="Cancel computation?" 
@@ -25,5 +47,7 @@ function cancelComputation() {
                @ok ="cancelComputation()">
         <p>Are you sure you want to cancel the active computation?</p>
       </b-modal>
+      <b-button @click="$emit('computing')">Computing</b-button>
+      <b-button @click="$emit('done')">Done</b-button>
     </div>
 </template>
