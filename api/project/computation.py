2bab8ee11ff5ee4974b6a9ed7a5c59c670e1fbb3
Lan Le <lanle@hotmail.nl>
2022-03-29 18:00:56 +0200

    show multiple items (datasets and approaches) in table

--- api/project/computation.py
+++ api/project/computation.py
@@ -13,12 +13,12 @@ compute_bp = Blueprint('computation', __name__, url_prefix='/computation')
 
 # constants
 DATASETS = [
-    {'name' : 'LFM2B', 'timestamp' : True, 'params' : {'filters' : ['age', 'gender', 'country']}}, 
-    {'name' : 'LFM1B', 'timestamp' : True, 'params' : {'filters' : ['age', 'gender', 'occupation']}},
-    {'name' : 'LFM360K', 'timestamp' : False, 'params' : {'filters' : ['age', 'gender', 'occupation']}},
-    {'name' : 'ML25M', 'timestamp' : True, 'params' : []},
-    {'name' : 'ML100K', 'timestamp' : True, 'params' : {'filters' : ['age', 'gender', 'occupation']}},
-    ]
+    {'name': 'LFM2B', 'timestamp': True, 'params': {'filters': ['age', 'gender', 'country']}},
+    {'name': 'LFM1B', 'timestamp': True, 'params': {'filters': ['age', 'gender', 'occupation']}},
+    {'name': 'LFM360K', 'timestamp': False, 'params': {'filters': ['age', 'gender', 'occupation']}},
+    {'name': 'ML25M', 'timestamp': True, 'params': []},
+    {'name': 'ML100K', 'timestamp': True, 'params': {'filters': ['age', 'gender', 'occupation']}},
+]
 APPROACHES = [
     {'name': 'ALS', 'params': {'values': [{'name': 'features', 'min': 1, 'max': 1000, 'default': 10}]}},
     {'name': 'POP',
@@ -29,11 +29,12 @@ OTHER_METRICS = ['DCG', 'RMSE', 'MAE', 'MRR', 'Item Coverage', 'Gini index']
 DEFAULTS = {'split': 80,
             'recCount': {'min': 0, 'max': 100, 'default': 10},
             }  # default values
-FILTERS = [{'name' : 'Artist Gender', 'params' : {'options' : [{'name':'Gender', 'options' : ['Male', 'Female']}]}},
-           {'name' : 'User Gender', 'params' : {'options' : [{'name':'Gender', 'options' : ['Male', 'Female']}]}},
-           {'name' : 'Country user threshold', 'params' : {'values' : [{'name' : 'threshold', 'min' : 1, 'max':1000, 'default':10}]}},
-           {'name' : 'Minimum age', 'params' : {'values' : [{'name' : 'threshold', 'min' : 1, 'max':1000, 'default':18}]}},
-           {'name' : 'Maximum age', 'params' : {'values' : [{'name' : 'threshold', 'min' : 1, 'max':1000, 'default':18}]}}]
+FILTERS = [{'name': 'Artist Gender', 'params': {'options': [{'name': 'Gender', 'options': ['Male', 'Female']}]}},
+           {'name': 'User Gender', 'params': {'options': [{'name': 'Gender', 'options': ['Male', 'Female']}]}},
+           {'name': 'Country user threshold',
+            'params': {'values': [{'name': 'threshold', 'min': 1, 'max': 1000, 'default': 10}]}},
+           {'name': 'Minimum age', 'params': {'values': [{'name': 'threshold', 'min': 1, 'max': 1000, 'default': 18}]}},
+           {'name': 'Maximum age', 'params': {'values': [{'name': 'threshold', 'min': 1, 'max': 1000, 'default': 18}]}}]
 
 computation_queue = []
 
@@ -77,23 +78,25 @@ def calculate():
         for dataset in datasets:
             recs = []
             for approach in settings['approaches']:
-                recommendation = {'recommendation': recommend(dataset, approach), 'evals': []}
+                recommendation = {'recommendation': recommend(dataset['name'], approach), 'evals': []}
                 for metric in settings['metrics']:
                     evaluation = evaluate(approach, metric)
                     recommendation['evals'].append({'name': metric['name'], 'evaluation': evaluation})
                 recs.append(recommendation)
             result.append({'dataset': dataset, 'recs': recs})
 
-        #result_storage.save_result(data.get('metadata'), settings, result)
-        
+        # result_storage.save_result(data.get('metadata'), settings, result)
+
     else:
         response['calculation'] = result_storage.newest_result()
         print(response)
     return response
 
-@compute_bp.route('/queue', methods=['GET','POST'])
+
+@compute_bp.route('/queue', methods=['GET', 'POST'])
 def queue():
     return json.dumps(computation_queue)
+
 
 @compute_bp.route('/queue/delete', methods=['POST'])
 def deleteItem():
@@ -102,6 +105,7 @@ def deleteItem():
     computation_queue.pop(index)
     return "Removed index"
 
+
 def recommend(dataset, approach):
     return dataset + approach['name'][::-1]  # Mock
 
@@ -109,10 +113,12 @@ def recommend(dataset, approach):
 def evaluate(approach, metric):
     return len(approach['name']) * len(metric['name']) * len(metric['parameter']['name'])  # Mock
 
-#add a computation request to the queue
+
+# add a computation request to the queue
 def append_queue(metadata, settings):
     timestamp = time.time()
     now = datetime.datetime.now()
     currentDt = now.strftime('%Y-%m-%dT%H:%M:%S') + ('-%02d' % (now.microsecond / 10000))
-    current_request = {'timestamp': {'stamp': timestamp, 'datetime' : currentDt}, 'metadata': metadata, 'settings': settings}
+    current_request = {'timestamp': {'stamp': timestamp, 'datetime': currentDt}, 'metadata': metadata,
+                       'settings': settings}
     computation_queue.append(current_request)
--- src/components/ActiveComputation.vue
+++ src/components/ActiveComputation.vue
@@ -1,5 +1,5 @@
 <script setup>
-    /*This program has been developed by students from the bachelor Computer Science at
+/*This program has been developed by students from the bachelor Computer Science at
     Utrecht University within the Software Project course.
     © Copyright Utrecht University (Department of Information and Computing Sciences)*/
 import Table from './Table.vue'
@@ -19,62 +19,56 @@ const headers = ref([
   { name: 'Metrics' },
 ])
 
-
 onMounted(() => {
   getComputations()
 })
 
-
 async function cancelComputation() {
-
   emit('stop')
 }
 
-async function getComputations(){
+async function getComputations() {
   const response = await fetch('http://localhost:5000/computation/queue')
   const data = await response.json()
 
   console.log(data)
   let allResults = data
   for (let i in allResults) {
-    let approach = allResults[i].settings.approaches[0]
-    let apprName = approach ? approach.name : 'NULL'
+    let approaches = allResults[i].settings.approaches
+    let apprName = approaches[0] ? approaches[0].name : 'NULL'
     let metric = allResults[i].settings.metrics[0]
     let metricName = metric ? metric.name : 'NULL'
 
     computations.value[i] = {
       datetime: allResults[i].timestamp.datetime,
       name: allResults[i].metadata.name,
-      dataset: allResults[i].settings.datasets[0],
-      approach: apprName,
+      datasets: allResults[i].settings.datasets,
+      approaches: approaches,
       metric: metricName,
-    }}
-  if(data != [])
-  {
-      emit('computing')
-      console.log(computations.value)
+    }
   }
-  else
-  {
-      emit('done')
-      alert('computations done!!!!')
+  if (data != []) {
+    emit('computing')
+    console.log(computations.value)
+  } else {
+    emit('done')
+    alert('computations done!!!!')
   }
 }
 
-var done;
-
+var done
 </script>
 
 <template>
-    <div>
-      <Table 
-        :results="computations" 
-        :headers="headers" 
-        :buttonText="'Cancel'" 
-        :removable="true" 
-        :serverFile="'/computation/queue/delete'" 
-        />
-      <b-button @click="$emit('computing')">Computing</b-button>
-      <b-button @click="$emit('done')">Done</b-button>
-    </div>
+  <div>
+    <Table
+      :results="computations"
+      :headers="headers"
+      :buttonText="'Cancel'"
+      :removable="true"
+      :serverFile="'/computation/queue/delete'"
+    />
+    <b-button @click="$emit('computing')">Computing</b-button>
+    <b-button @click="$emit('done')">Done</b-button>
+  </div>
 </template>
