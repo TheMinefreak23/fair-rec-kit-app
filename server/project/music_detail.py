3c5d09f945556cf42e50a6989d2e875d4046cd93
AlexC123456 <aleksej.cornelissen@hotmail.com>
2022-05-07 01:03:18 +0200

    added background generation

--- server/project/music_detail.py
+++ server/project/music_detail.py
@@ -6,8 +6,12 @@ Utrecht University within the Software Project course.
 import base64
 import json
 import time
-
+from turtle import back
 import requests as requests
+import numpy as np
+
+from PIL import Image
+from io import BytesIO
 from flask import Blueprint, request
 
 detail_bp = Blueprint('music', __name__, url_prefix='/api/music')
@@ -37,12 +41,48 @@ def get_spotify_token():
 
 @detail_bp.route('/AcousticBrainz', methods=['POST'])
 def get_acousticbrainz_data():
+    """Requests High-Level audio features from the AcousticBrainz API using a MusicBrainzID
+
+    Returns:
+        Dict: Dictionary with all High-Level audiofeatures
+    """
     data = request.get_json()
     print(data)
     ab_api = 'https://acousticbrainz.org/api/v1/'
     musicbrainz_id = data.get('mbid')
     url = ab_api + 'high-level' + '?recording_ids=' + musicbrainz_id
-    print(url)
     res = requests.get(url)
-    print(res.status_code)
     return json.loads(res.text)
+
+@detail_bp.route('/background', methods = ["GET"])
+def get_background():
+    """Generates a background from the album covers of a Spotify Playlist
+
+    Returns:
+        Image: Background-image
+    """``
+    playlist_id = '37i9dQZEVXbMDoHDwVN2tF'
+    url = 'https://api.spotify.com/v1/playlists/' + playlist_id + '/tracks?limit=50'
+    headers = {'Authorization': 'Bearer ' + token['access_token'], 'Content-Type': 'application/json'}
+    res = requests.get(url, headers=headers)
+    if(res.status_code == 401):
+        get_spotify_token()
+        get_background()
+    playlist = json.loads(res.text)
+    images = []
+    for item in (playlist['items']):
+        response = requests.get(item['track']['album']['images'][1]['url'])
+        img = Image.open(BytesIO(response.content))
+        images.append(img)
+    background = Image.new('RGB', (3000, 3000))
+    w, h = background.size
+    index = 0
+    for i in range(0, w, 300):
+        for j in range(0, h, 300):
+            background.paste(images[index], (i,j))
+            index+=1
+            if(index == 49):
+                np.flip(images)
+                index = 0
+    background.save('../background.png')
+    return background
