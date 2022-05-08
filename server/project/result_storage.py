d15841717e2ebb0849c01decd69c6968b84d190d
Lan Le <lanle@hotmail.nl>
2022-05-08 09:01:50 +0200

    lost table changes, python docstring formatting

--- server/project/result_storage.py
+++ server/project/result_storage.py
@@ -15,11 +15,11 @@ RESULTS_OVERVIEW_PATH = 'results.json'
 
 
 def save_result(computation, result):
-    """
-    Save result to overview.
+    """Save result to overview.
 
-    :param computation: the computation settings
-    :param result: the computed result
+    Args:
+        computation(dict): the computation settings
+        result(dict): the computed result
     """
     global current_result
     computation['result'] = result
@@ -34,11 +34,10 @@ def save_result(computation, result):
 
 
 def result_by_id(result_id):
-    """
-    Get a result from the results overview by its id.
+    """Set the current result to a result in the results overview by its id.
 
-    :param result_id: the result id
-    :return: the corresponding result (settings, recs, evaluation)
+    Args:
+        result_id(int): the result id
     """
     results = load_json(RESULTS_OVERVIEW_PATH)
 
@@ -57,21 +56,22 @@ def result_by_id(result_id):
 
 
 def load_json(path):
-    """
-    Load a JSON file to a dictionary using its path.
+    """Load a JSON file to a dictionary using its path.
 
-    :param path: the path to the JSON file
-    :return: the JSON as a dictionary
+    Args:
+        path(string): the path to the JSON file
+    Returns:
+        (dict) the JSON as a dictionary
     """
     with open(path, 'r', encoding='utf-8') as file:
         return json.load(file)  # Load existing data into a dict.
 
 
 def load_results_overview():
-    """
-    Load the results overview.
+    """Load the results overview.
 
-    :return: the loaded results
+    Returns:
+        (dict) the loaded results
     """
     # Ensure the results overview exists.
     create_results_overview()
@@ -79,10 +79,10 @@ def load_results_overview():
 
 
 def write_results_overview(results):
-    """
-    Write (store) results to the overview.
+    """Write (store) results to the overview.
 
-    :param results: the results to store
+    Args:
+        results(dict): the results to store
     """
     # Open the file in write mode.
     with open(RESULTS_OVERVIEW_PATH, 'w', encoding='utf-8') as file:
@@ -91,10 +91,10 @@ def write_results_overview(results):
 
 
 def add_result(result):
-    """
-    Add a result at the end of the results overview.
+    """Add a result at the end of the results overview.
 
-    :param result: the (new) result
+    Args:
+        result(dict): the (new) result
     """
     file_results = load_results_overview()
     file_results['all_results'].append(result)
@@ -102,10 +102,10 @@ def add_result(result):
 
 
 def delete_result(index):
-    """
-    Delete a result by its index.
+    """Delete a result by its index.
 
-    :param index: the index of the result
+    Args:
+        index(int): the index of the result
     """
     file_results = load_results_overview()
     file_results['all_results'].pop(index)
@@ -113,13 +113,13 @@ def delete_result(index):
 
 
 def edit_result(index, new_name, new_tags, new_email):
-    """
-    Edit a result.
+    """Edit a result.
 
-    :param index: the result index
-    :param new_name: the new metadata name of the result
-    :param new_tags: the new metadata tags of the result
-    :param new_email: the new metadata email of the result
+    Args:
+        index(int): the result index
+        new_name(string): the new metadata name of the result
+        new_tags(string): the new metadata tags of the result
+        new_email(string): the new metadata email of the result
     """
     file_results = load_results_overview()
     to_edit_result = file_results['all_results'][index]
@@ -148,11 +148,13 @@ def create_results_overview():
 
 
 def parse_tags(tags_string):
-    """
-    Parse result tags (given by user as metadata)
+    """Parse result tags (given by user as metadata)
 
-    :param tags_string: the tags as raw string input (comma-separated)
-    :return: the split list of tags
+    Args:
+        tags_string(string): the tags as raw string input (comma-separated)
+
+    Returns:
+        the split list of tags
     """
     # Split tags by comma and get the unique tags
     unique = list(set(tags_string.split(',')))
