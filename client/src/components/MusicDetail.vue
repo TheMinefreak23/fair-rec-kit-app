6d5de209de570103ca4a65bdce91d1d7a779dba5
AlexC <aleksej.cornelissen@hotmail.com>
2022-05-18 13:02:07 +0200

    wallpaper blur

--- client/src/components/MusicDetail.vue
+++ client/src/components/MusicDetail.vue
@@ -1,8 +1,8 @@
 <script setup>
-import { onMounted, ref } from 'vue'
-import { API_URL } from '../api'
-import { getSpotifyToken, getSongInfo } from '../helpers/songInfo'
-import { Bar } from 'vue-chartjs'
+import { onMounted, ref } from "vue";
+import { API_URL } from "../api";
+import { getSpotifyToken, getSongInfo } from "../helpers/songInfo";
+import { Bar } from "vue-chartjs";
 import {
   Chart as ChartJS,
   Title,
@@ -11,22 +11,29 @@ import {
   BarElement,
   CategoryScale,
   LinearScale,
-} from 'chart.js'
+} from "chart.js";
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+);
 
-const token = ref('test')
-const tracks = ref([])
-const track = ref({})
-const trackModalShow = ref(false)
-const query = ref({ track: 'orion', artist: 'metallica' })
-const songInfo = ref()
-const chartInfo = ref({ labels: [], datasets: [] })
+const token = ref("test");
+const tracks = ref([]);
+const track = ref({});
+const trackModalShow = ref(false);
+const query = ref({ track: "orion", artist: "metallica" });
+const songInfo = ref();
+const chartInfo = ref({ labels: [], datasets: [] });
 
 onMounted(async () => {
-  token.value = await getSpotifyToken()
-  getInfo()
-})
+  token.value = await getSpotifyToken();
+  getInfo();
+});
 
 // Get music detail info
 async function getInfo() {
@@ -34,32 +41,47 @@ async function getInfo() {
     token.value,
     query.value.track,
     query.value.artist
-  )
+  );
 
-  tracks.value = await songInfo.value.Spotify
-  track.value = tracks.value.items[0]
+  tracks.value = await songInfo.value.Spotify;
+  track.value = tracks.value.items[0];
   //get AcousticBrainz highlevel features using LastFM's mbid
-  const highlevelFeatures = await songInfo.value.AcousticBrainz[songInfo.value.LastFM.track.mbid][0]['highlevel']
+  const highlevelFeatures = await songInfo.value.AcousticBrainz[
+    songInfo.value.LastFM.track.mbid
+  ][0]["highlevel"];
 
-  await generateChart(await highlevelFeatures)
-
+  await generateChart(await highlevelFeatures);
 }
 
 //Generate the data for the audiofeatures Bar-chart
 async function generateChart(highlevelFeatures) {
   //available moods from AcousticBrainz
-  const moods = ['acoustic', 'aggressive', 'electronic', 'happy', 'party', 'relaxed', 'sad']
+  const moods = [
+    "acoustic",
+    "aggressive",
+    "electronic",
+    "happy",
+    "party",
+    "relaxed",
+    "sad",
+  ];
   //also include danceability seperately
-  const danceability = highlevelFeatures['danceability']['all']['danceable']
-  const data = [danceability]
+  const danceability = highlevelFeatures["danceability"]["all"]["danceable"];
+  const data = [danceability];
   for (const mood of moods) {
-    const tagname = 'mood_' + mood
-    const feature = highlevelFeatures[(tagname)]
-    console.log('value', feature)
-    data.push(feature.all[mood])
+    const tagname = "mood_" + mood;
+    const feature = highlevelFeatures[tagname];
+    console.log("value", feature);
+    data.push(feature.all[mood]);
   }
-  chartInfo.value.datasets[0] = ({ 'label': 'Attributes', 'backgroundColor': "#000080", 'data': data })
-  chartInfo.value.labels = ['danceability'].concat(moods.map(mood => mood + '-ness'))
+  chartInfo.value.datasets[0] = {
+    label: "Attributes",
+    backgroundColor: "#000080",
+    data: data,
+  };
+  chartInfo.value.labels = ["danceability"].concat(
+    moods.map((mood) => mood + "-ness")
+  );
 }
 </script>
 
@@ -71,12 +93,18 @@ async function generateChart(highlevelFeatures) {
         <b-row>
           <b-col>
             <b-form-group label="Track">
-              <b-form-input v-model="query.track" placeholder="Enter track name" />
+              <b-form-input
+                v-model="query.track"
+                placeholder="Enter track name"
+              />
             </b-form-group>
           </b-col>
           <b-col>
             <b-form-group label="Artist">
-              <b-form-input v-model="query.artist" placeholder="Enter artist name" />
+              <b-form-input
+                v-model="query.artist"
+                placeholder="Enter artist name"
+              />
             </b-form-group>
           </b-col>
           <b-col>
@@ -95,62 +123,106 @@ async function generateChart(highlevelFeatures) {
     </b-card>
 
     <template v-if="track">
-      <b-button style="width: 20vw; display: block" class="mx-auto" variant="primary"
-        @click="trackModalShow = !trackModalShow">Show track
+      <b-button
+        style="width: 20vw; display: block"
+        class="mx-auto"
+        variant="primary"
+        @click="trackModalShow = !trackModalShow"
+        >Show track
       </b-button>
-      <b-modal :style="{
-        backgroundColor: 'black',
-        color: 'black',
-      }" v-if="track.artists" v-model="trackModalShow" :title="track.name + ' by ' + track.artists[0].name"
-        size="lg">
-        <b-container class="p-3">
-          <b-row v-if="track.album">
-            <b-row class="p-3">
-              <b-col>
-                <p>
-                  <!--TODO refactor into component with dynamic formatting-->
-                  Artist(s):
-                  <template v-for="(artist, index) in track.artists">{{
+      <b-modal
+        static
+        :style="{
+          backgroundColor: 'black',
+          color: 'black',
+        }"
+        v-if="track.artists"
+        v-model="trackModalShow"
+        :title="track.name + ' by ' + track.artists[0].name"
+        size="lg"
+      >
+      <div class="background-image"/>
+        <div class="content">
+          <b-container class="p-3">
+            <b-row v-if="track.album">
+              <b-row class="p-3">
+                <b-col>
+                  <p>
+                    <!--TODO refactor into component with dynamic formatting-->
+                    Artist(s):
+                    <template v-for="(artist, index) in track.artists">{{
                       artist.name
-                  }}</template>
-                </p>
-                <p>Album: {{ track.album.name }}</p>
-                <Bar :chartData="chartInfo">
-                </Bar>
-                <p v-html="songInfo.LastFM.track.wiki.summary"></p>
-                <p>LastFM tags:</p>
-                <div v-for="item in songInfo.LastFM.track.toptags.tag">
-                  <a :href="item.url">{{ item.name }}</a>
-                </div>
-              </b-col>
-              <b-col>
-                <!--Using medium sized image-->
-                <img :src="track.album.images[1].url" />
-              </b-col>
+                    }}</template>
+                  </p>
+                  <p>Album: {{ track.album.name }}</p>
+                  <Bar :chartData="chartInfo"> </Bar>
+                  <p v-html="songInfo.LastFM.track.wiki.summary"></p>
+                  <b>LastFM tags:</b>
+                  <div v-for="item in songInfo.LastFM.track.toptags.tag">
+                    <a :href="item.url">{{ item.name }}</a>
+                  </div>
+                </b-col>
+                <b-col>
+                  <!--Using medium sized image-->
+                  <img :src="track.album.images[1].url" />
+                </b-col>
+              </b-row>
             </b-row>
-          </b-row>
-          <b-row class="p-3">
-            <iframe style="border-radius: 12px" :src="
-              'https://open.spotify.com/embed/track/' +
-              track.id +
-              '?utm_source=generator'
-            " width="100%" height="80" frameBorder="0" allowfullscreen=""
-              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" />
-          </b-row>
-          <b-row class="p-3">
-            <b-button v-b-toggle.collapse-1 variant="primary">Show full info</b-button>
-            <b-collapse id="collapse-1">
-              <h3>
-                debug | id: {{ track.id }} | preview url:
-                {{ track.preview_url }}
-              </h3>
-              <p v-for="[key, value] of Object.entries(track)">
-                <b>{{ key }}</b>: {{ value }}
-              </p>
-            </b-collapse>
-          </b-row>
-        </b-container>
+            <b-row class="p-3">
+              <iframe
+                style="border-radius: 12px"
+                :src="
+                  'https://open.spotify.com/embed/track/' +
+                  track.id +
+                  '?utm_source=generator'
+                "
+                width="100%"
+                height="80"
+                frameBorder="0"
+                allowfullscreen=""
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              />
+            </b-row>
+            <b-row class="p-3">
+              <b-button v-b-toggle.collapse-1 variant="primary"
+                >Show raw info</b-button
+              >
+              <b-collapse id="collapse-1">
+                <h3>
+                  debug | id: {{ track.id }} | preview url:
+                  {{ track.preview_url }}
+                </h3>
+                <p v-for="[key, value] of Object.entries(track)">
+                  <b>{{ key }}</b
+                  >: {{ value }}
+                </p>
+              </b-collapse>
+            </b-row>
+          </b-container>
+        </div>
       </b-modal>
     </template>
   </b-container>
 </template>
+
+<style scoped>
+.background-image {
+  position: fixed;
+  left: 0;
+  right: 0;
+  z-index: 1;
+  display: block;
+  background-image: url('public/background.png');
+  background-size: 100%;
+  filter: blur(8px);
+  opacity: 0.5
+  }
+  .content {
+  position: fixed;
+  left: 0;
+  right: 0;
+  z-index: 9999;
+  margin-left: 20px;
+  margin-right: 20px;
+}
+</style>
