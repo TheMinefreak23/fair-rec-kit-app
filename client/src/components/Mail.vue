8ee8ef3e90766a3b7b3294bb8590372b09c57464
r2dvos <voswesseling@gmail.com>
2022-05-25 13:11:13 +0200

    mail

---
+++ client/src/components/Mail.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue'
+//import Message from flask_mail
+
+const sender = ref('')
+const receiving = ref('')
+const message = ref('')
+</script>
+
+
+<template>
+    <b-container>
+        <h2 class="text-center">Sending a mail</h2>
+        <b-row>
+            <b-col>
+                <h6>Sender:</h6>
+                <b-form-input
+                    v-model="sender"
+                    type="email"
+                />
+            </b-col>
+            <b-col>
+                <h6>Receiver:</h6>
+                <b-form-input
+                    v-model="receiving"
+                    type="email"
+                />
+            </b-col>
+            <b-col>
+            </b-col>
+        </b-row>
+        <h6>Message:</h6>
+        <b-row>
+            <b-form-input
+                v-model="message"
+                type="text"
+            />
+        </b-row>
+    </b-container>
+</template>
--- server/project/__init__.py
+++ server/project/__init__.py
@@ -5,6 +5,7 @@ Utrecht University within the Software Project course.
 """
 from flask import Flask
 from flask_cors import CORS
+from flask_mail import Mail, Message
 
 from .experiment import compute_bp
 from .music_detail import detail_bp
@@ -18,7 +19,27 @@ def create_app():
     app = Flask(__name__)
     app.config['ENV'] = 'development'
     app.config['DEBUG'] = True
-    app.config['TESTING'] = True
+    app.config['TESTING'] = False
+
+    # Mail stuff
+    app.config['MAIL_SERVER']='smtp.mailtrap.io'
+    app.config['MAIL_PORT'] = 2525
+    app.config['MAIL_USERNAME'] = '54587a9324d744'
+    app.config['MAIL_PASSWORD'] = '0da1c03c71700d'
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USE_SSL'] = False
+    # app.config['MAIL_SUPRESS_SEND'] = False
+    mail = Mail(app)
+    
+    @app.route('/mail')
+    def index():
+        msg = Message(subject = 'foobar',
+                      body = 'I am calling to let you know about your cars expired warrenty please get back to me as soon as possible thanks :):):):)',
+                      sender='foo@bar.gov',
+                      recipients=['gaypiss6969@gmail.com'])
+        mail.send(msg)
+        #print(msg)
+        return 'owo'
 
     # Enable Cross-Origin Resource Sharing.
     CORS(app)
