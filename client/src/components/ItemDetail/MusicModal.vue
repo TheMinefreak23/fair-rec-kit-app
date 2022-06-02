934af6e0704cf6c1afcf86032c00b99185a5f065
RobVer <58036019+RobVer@users.noreply.github.com>
2022-06-02 16:16:15 +0200

    restyled entire musicmodal

--- client/src/components/ItemDetail/MusicModal.vue
+++ client/src/components/ItemDetail/MusicModal.vue
@@ -56,7 +56,7 @@ onMounted(async () => {
 
 // Generate the data for the audiofeatures Bar-chart
 async function generateChart(highlevelFeatures) {
-  chartInfo.value = { labels: [], datasets: [] }
+  chartInfo.value = { labels: [], datasets: [], options: {responsive: true}}
   // available moods from AcousticBrainz
   const moods = [
     'acoustic',
@@ -79,7 +79,8 @@ async function generateChart(highlevelFeatures) {
   chartInfo.value.datasets[0] = {
     label: 'Attributes',
     color: 'white',
-    backgroundColor: 'orange',
+    backgroundColor: '#017C8E',
+    responsive: true,
     data: data,
   }
   chartInfo.value.labels = ['danceability'].concat(
@@ -90,87 +91,91 @@ async function generateChart(highlevelFeatures) {
 
 <template>
   <b-modal
-    body-bg-variant="secondary"
+    body-bg-variant="white"
+    body-padding="0"
     header-bg-variant="dark"
     footer-bg-variant="dark"
-    body-text-variant="white"
+    body-text-variant="black"
     header-text-variant="white"
-    footer-text-variant="white"
+    footer-text-variant="black"
     v-if="track.artists"
     v-model="modalShow"
     :title="track.name + ' - ' + track.artists[0].name"
     size="lg"
+    hide-footer
   >
-    <div class="wrap">
-      <div class="content">
-        <b-container class="p-3">
-          <b-card style="background-color: rgba(0, 0, 0, 0.6)">
-            <b-row class="p-3 align-middle">
+    <div class="modal-body p-0">
+      <div class="content bg-white p-0">
+        <b-container class="p-0">
+            <b-row class="p-3 align-middle rounded-3 mx-1 bg-secondary">
               <b-col cols="6" class="text-center">
                 <!--Using medium sized image-->
-                <img :src="track.album.images[1].url" style="height: 250px;"/>
+                <img :src="track.album.images[1].url" style="width: 80%;"/>
               </b-col>
               <b-col>
-                <h1>{{ track.name }}</h1>
-                <h2>
+                <h2>{{ track.name }}</h2>
+                <h4 class="fst-italic">
                   <!--TODO refactor into component with dynamic formatting-->
-                  Artist(s):
                   <template v-for="artist in track.artists">{{
                     artist.name
                   }}</template>
-                </h2>
+                </h4>
                 <h4>Album: {{ track.album.name }}</h4>
+                <AudioSnippet :trackId="track.id" class="mt-2"/>
               </b-col>
             </b-row>
 
-            <b-row class="p-3">
+            <b-row v-if="lastFmTrack" class="mt-2 mx-1">
+              <!-- Tags -->
+              <b-col class="p-0">
+                <b-card class="bg-secondary border-0">
+                  <b>LastFM tags:</b>
+                  <b-badge variant="dark" v-for="item in lastFmTrack.toptags.tag" :key="item" class="d-inline ms-2 p-2">
+                    <a :href="item.url" class="text-white" style="font-size: 14px;">{{ item.name }}</a>
+                  </b-badge>
+                </b-card>
+              </b-col>
+            </b-row>
+
+            <b-row v-if="lastFmTrack" class="mt-2 mx-1">
+              <!-- Wiki summary -->
+              <b-col class="p-0 rounded-end-0">
+                <b-card v-if="lastFmTrack.wiki" class="bg-secondary border-0">
+                  <p v-html="lastFmTrack.wiki.summary"></p>
+                </b-card>
+                <b-card v-else class="bg-secondary border-0">
+                  <i>No wiki available</i>
+                </b-card>
+              </b-col>
+            </b-row>
+
+            <b-row class="mt-2 mx-1 bg-secondary">
               <!-- Graph and description -->
-              <b-col cols="6">
+              <b-col class="p-0 bg-secondary">
                 <!-- Chart -->
                 <b-card
                   v-if="chartInfo"
-                  :style="{
-                    backgroundColor: 'rgba(0, 0, 0, 0.9)',
-                    opacity: 0.9,
-                  }"
-                >
-                  <Bar :chartData="chartInfo"> </Bar>
-                </b-card>
-                <h4 v-else>(No chart available)</h4>
-              </b-col>
-            </b-row>
-            <b-row v-if="lastFmTrack">
-              <!-- Wiki summary -->
-              <b-col>
-                <b-card v-if="lastFmTrack.wiki" class="bg-dark">
-                  <p v-html="lastFmTrack.wiki.summary"></p>
-                </b-card>
-                <h4 v-else>(No wiki available)</h4>
-              </b-col>
-              <!-- Tags -->
-              <b-col>
-                <b-card class="bg-dark">
-                  <b>LastFM tags:</b>
-                  <div v-for="item in lastFmTrack.toptags.tag" :key="item">
-                    <a :href="item.url" class="text-info">{{ item.name }}</a>
+                  class="bg-secondary border-0"
+                > 
+                  <div>
+                    <Bar :chartData="chartInfo"> </Bar>
                   </div>
                 </b-card>
+                <b-card v-else class="bg-secondary border-0">
+                  <i>No chart available</i>
+                </b-card>
               </b-col>
             </b-row>
-            <b-row class="p-3">
-              <AudioSnippet :trackId="track.id" />
-            </b-row>
-          </b-card>
-          <b-row class="p-3">
+          <b-row class="py-2 mx-1">
             <b-button v-b-toggle.collapse-1 variant="primary"
-              >Show raw info</b-button
+              >Toggle show raw info</b-button
             >
             <b-collapse id="collapse-1">
-              <h3>
-                debug | id: {{ track.id }} | preview url:
-                {{ track.preview_url }}
-              </h3>
-              <p v-for="[key, value] of Object.entries(track)" :key="key">
+              <h6 class="mt-1">
+                debug | id: <i>{{ track.id }}</i><br>
+                preview url: <a :href='track.preview_url'>{{ track.preview_url }}</a>
+              </h6>
+              <p class="m-0" v-for="[key, value] of Object.entries(track)" :key="key">
                 <b>{{ key }}</b
                 >: {{ value }}
               </p>
@@ -182,28 +187,3 @@ async function generateChart(highlevelFeatures) {
   </b-modal>
 </template>
 
-<style scoped>
-.wrap {
-  position: relative;
-  background-color: black;
-}
-
-.wrap:before {
-  content: ' ';
-  display: block;
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  opacity: 0.3;
-  filter: blur(1px);
-  background-repeat: repeat;
-  background-position: 50% 0;
-  background-size: 100%;
-}
-
-.content {
-  position: relative;
-}
-</style>
