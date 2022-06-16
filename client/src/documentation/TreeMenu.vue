59134fe1564c55d6b6a107bcf8d325fa12256fed
RobVer <58036019+RobVer@users.noreply.github.com>
2022-06-16 17:59:30 +0200

    attempt at fixing dropdown buttons

--- client/src/documentation/TreeMenu.vue
+++ client/src/documentation/TreeMenu.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="tree-menu">
     <b-card class="border-end-0 border-top-0 border-bottom-0 border-5 bg-secondary">
-      <b-card-title>{{ label }} 
-        <b-button> 
-          <template> <i class="bi bi-caret-down" /> </template>
-        </b-button>
-      </b-card-title>
+      <b-button  v-b-toggle="label" variant="primary">{{label}}</b-button>
       <!-- v-if because if there is no description -> undefined, which takes space. -->    
-      <tree-menu 
-        v-for="node in nodes" 
-        :nodes="node.nodes" 
-        :label="node.label"
-        :depth="depth+1"
-      >
-      </tree-menu>
+      <b-collapse :id='label' class="collapse show">
+        <tree-menu 
+          v-for="node in nodes" 
+          :nodes="node.nodes" 
+          :label="node.label"
+          :depth="depth+1"
+        >
+        </tree-menu>
+      </b-collapse>
     </b-card> 
   </div>
 </template>
