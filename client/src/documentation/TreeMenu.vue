f60515f76b896e7cb687860558221ba633b0ecb1
RobVer <58036019+RobVer@users.noreply.github.com>
2022-06-16 16:45:31 +0200

    updated template with cards

--- client/src/documentation/TreeMenu.vue
+++ client/src/documentation/TreeMenu.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="tree-menu">
-    <div>{{ label }}</div>
-    <tree-menu 
-      v-for="node in nodes" 
-      :nodes="node.nodes" 
-      :label="node.label"
-    >
-    </tree-menu>
+    <b-card class="border-end-0 border-top-0 border-bottom-0 border-5 bg-secondary">
+      <b-card-title>{{ label }} 
+        <b-button> 
+          <template> <i class="bi bi-caret-down" /> </template>
+        </b-button>
+      </b-card-title>
+      <!-- v-if because if there is no description -> undefined, which takes space. -->    
+      <tree-menu 
+        v-for="node in nodes" 
+        :nodes="node.nodes" 
+        :label="node.label"
+        :depth="depth+1"
+      >
+      </tree-menu>
+    </b-card> 
   </div>
 </template>
 <script>
