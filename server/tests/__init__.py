cbdbb4258ef7ed1cf8f73b387726336168831ab9
OGKushper <casperthijsen@hotmail.nl>
2022-06-16 14:42:30 +0200

    Added modules to init

--- server/tests/__init__.py
+++ server/tests/__init__.py
@@ -1,4 +1,29 @@
 """
+Experiment_bp.py: It is used to get the available options for the experiments,
+and it handles the experiments by adding them to the queue.
+There is also a method to get the current queue
+and there is also a function that handles the abortion or cancellation of an experiment.
+
+Music_detail_bp.py: This file handles getting the music details that are shown in the tables in the result.
+
+Result_bp.py: This file handles everything for getting a specific result.
+
+Results_bp.py: The methods in this file call functions that are in the models folder to load and edit results.
+
+Events.py: This file handles the events that are caused by the library, for example when an experiment is finished.
+
+Experiment.py: This file defines different classes required to handle the progress of experiments
+and the experiment itself, which in turn has methods to run new experiments or validate them.
+
+Experiment_queue.py: This contains a class that handles the queueing of the experiments.
+
+Mail.py: This contains a class that is used to handle sending emails to the user whenever an experiment is finished.
+
+Result_loader.py: This module contains functions to load and modify data from evaluation results.
+
+Result_storage.py: This module contains functions to load, save and modify data from the result overview.
+
+
 This program has been developed by students from the bachelor Computer Science at
 Utrecht University within the Software Project course.
 © Copyright Utrecht University (Department of Information and Computing Sciences)
