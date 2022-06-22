a8185785b228c3133e55d9c401e1cc62adaffc35
Lan Le <lanle@hotmail.nl>
2022-06-23 01:51:08 +0200

    CLIENT: hotfix train/test in settings modal

--- server/project/models/options_formatter.py
+++ server/project/models/options_formatter.py
@@ -293,9 +293,9 @@ class OptionsFormatter:
             dataset['splitting']['test_ratio'] = (100 -
                                                   int(dataset['params']['Train/testsplit'])
                                                   ) / 100
-            
-            #dataset['splitting']['Train/testsplit'] = str(dataset['params']['Train/testsplit']) \
-            #+ '/' + str(100 - dataset['params']['Train/testsplit'])
+
+            dataset['splitting']['train/testsplit'] = str(dataset['params']['Train/testsplit']) \
+            + '/' + str(100 - dataset['params']['Train/testsplit'])
 
             del dataset['params']['Train/testsplit']
 
